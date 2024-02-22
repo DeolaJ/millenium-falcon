e5291c874e6ce506dd6af9aae61e2d6068814cfa
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2024-02-22 14:57:46 +0000

    update: create types for people
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export type PeopleType = {
+    name: string;
+    birth_year: string;
+    eye_color: string;
+    gender: string;
+    hair_color: string;
+    height: string;
+    mass: string;
+    skin_color: string;
+    homeworld: string;
+    films: string[];
+    species: string[];
+    starships: string[];
+    vehicles: string[];
+    url: string;
+    created: Date;
+    edited: Date;
+};
+
+export type PeopleResponseType = {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: PeopleType[];
+};
