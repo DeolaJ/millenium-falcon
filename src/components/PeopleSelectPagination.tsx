edd23b0b81bc579cd9e3bf2785fec0161842110c
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2024-02-25 19:29:17 +0000

    feat: create pagination for people select component to navigate form stages
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ src/components/PeopleSelectPagination.tsx
@@ -0,0 +1,50 @@
+import { ImArrowLeft2, ImArrowRight2 } from "react-icons/im";
+
+import "./Pagination.scss";
+
+function PeopleSelectPagination({
+    nextText,
+    canPreviousStage,
+    next,
+    previous,
+    errorMessage,
+}: {
+    next: () => void;
+    previous?: () => void;
+    canPreviousStage?: boolean;
+    nextText?: string;
+    errorMessage?: string;
+}) {
+    return (
+        <div className="people-select--pagination--wrapper">
+            {errorMessage && (
+                <div className="pagination--error">
+                    <p>{errorMessage}</p>
+                </div>
+            )}
+            <div className={`pagination ${!canPreviousStage ? "flex-end" : ""}`}>
+                {canPreviousStage && (
+                    <button
+                        onClick={previous}
+                        className="pagination--button"
+                        aria-label="previous page"
+                    >
+                        <ImArrowLeft2
+                            className="pagination--button-icon previous-icon"
+                            aria-hidden
+                        />
+                        Previous
+                    </button>
+                )}
+
+                <button className="pagination--button" onClick={next} aria-label="next page">
+                    {nextText || "Next"}
+                    <ImArrowRight2 className="pagination--button-icon next-icon" aria-hidden />
+                    {errorMessage && <span className="sr-only">{errorMessage}</span>}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default PeopleSelectPagination;
