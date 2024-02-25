04a2c69bfd640375f6bad7e6feff9680ad387663
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2024-02-25 19:39:17 +0000

    update: add millenium form to project
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import MilleniumFalcon from "./components/MilleniumFalcon";
+
 function App() {
-    return <main>Let's go</main>;
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            }),
+    );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <MilleniumFalcon />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
